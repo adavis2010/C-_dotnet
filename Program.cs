13584520392ba669e312f919c81ac7f638ff79df
adavis2010 <78383492+adavis2010@users.noreply.github.com>
2021-02-11 20:54:09 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Practice {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Practing C# at home");
+            Console.WriteLine("Curly braces are a pain in the butt");
+
+            var firstname = "Andrea";
+            var lastname = "Davis";
+            var fullname = firstname + " " + lastname;
+            Console.WriteLine(fullname);
+            {
+                var subjects = new string[3];
+                subjects[0] = "SQL";
+                subjects[1] = "Git/Github";
+                subjects[2] = "C#";
+                Console.WriteLine("SQL, Git/GitHub, C#");
+
+                bool oneplusone = true;
+                bool isActive = true;
+
+
+            }
+
+            
+
+
+               
+            }
+            
+
+
+        }
+  
+    }  
+    
+
